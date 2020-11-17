736a88c0bca22f8b3c566869803a4be374f9df12
Ivan Zhuravlev <i.zhuravlev@gmail.com>
2020-11-17 23:06:17 +0300

    push components to files

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+const Header = () => {
+    return <header className='header'>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/f/fa/Apple_logo_black.svg"/>
+    </header>
+}
+
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+const Navbar = () => {
+    return <nav className='nav'>
+        <div>
+            <a>profile</a>
+        </div>
+        <div>
+            <a>Messages</a>
+        </div>
+        <div>
+            <a>Music</a>
+        </div>
+        <div>
+            <a>Settings</a>
+        </div>
+    </nav>
+}
+
+
+export default Navbar;
