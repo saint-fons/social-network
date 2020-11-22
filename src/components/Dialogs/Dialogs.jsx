669f875c13f1978a5d6c3844404a6a740333b362
Ivan Zhuravlev <i.zhuravlev.wok@gmail.com>
2020-11-22 16:26:16 +0300

    dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
-import styles from './Dialogs.module.css'
+import style from './Dialogs.module.css'
+import {NavLink} from "react-router-dom";
 
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={style.dialogs}>
+            <div className={style.items}>
+                <div className={style.dialog}>
+                    <NavLink to="/dialogs/1">Alexandra</NavLink>
+                </div>
+                <div className={style.dialog}>
+                    <NavLink to="/dialogs/2">Drmitri</NavLink>
+                </div>
+                <div className={style.dialog}>
+                    <NavLink to="/dialogs/3">Svetlana</NavLink>
+                </div>
+            </div>
+            <div className="messages">
+                <div className="message">hello</div>
+                <div className="message">how a u</div>
+                <div className="message">yoy</div>
+            </div>
         </div>
     )
 };
