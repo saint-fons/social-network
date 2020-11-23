a99ff417c57963c96c9160ff64eb10f8d16a14ea
Ivan Zhuravlev <i.zhuravlev.wok@gmail.com>
2020-11-23 23:43:11 +0300

    fonts

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import style from './Home.module.css'
 
 const Header = () => {
     return(
         <div>
             <img src='https://upload.wikimedia.org/wikipedia/commons/4/47/Go-home-2.svg' alt='home' />
-            <h6 className="hello">
+            <h6 className={style.hello}>
                 Hello this is my first project.
                 U can try functions from navigation bar. plz check registration, and profiles
             </h6>
